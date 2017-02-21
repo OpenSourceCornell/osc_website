79452f844c43cde53ef5c974f80c54b9c39f07d5
David Li <li.davidm96@gmail.com>
2017-02-21 16:41:51 -0500

    Use `jekyll serve` to work around html-proofer testing permalinks

--- scripts/ci_test.sh
+++ scripts/ci_test.sh
@@ -3,6 +3,17 @@
 # Halt the script if an error occurs
 set -e
 
+# Build the site once to be sure
 bundle exec jekyll build
+
+# We use `jekyll serve` to get around the permalink
+# problem. html-proofer checks non-relative links. However, our site
+# uses absolute links to serve as permalinks. At CI testing time,
+# these permalinks will not exist. Instead, we run `jekyll serve`, so
+# that the "permalinks" are now links to the testing server.
+bundle exec jekyll serve &
+
+# Give Jekyll time to spin up
+sleep 5
 
 bundle exec htmlproofer ./_site
